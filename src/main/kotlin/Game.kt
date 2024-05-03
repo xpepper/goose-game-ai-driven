034042b92443a82ca273c5dea20a0abe4a1ab635
Pietro Di Bello <pierodibello@gmail.com>
2024-05-03 23:45:38 +0200

    refactor

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -2,12 +2,11 @@ class Game {
     private val players = mutableListOf<String>()
 
     fun addPlayer(name: String) {
-        if (!players.contains(name)) {
-            players.add(name)
+        if (name in players) {
+            return // Consider logging or handling this case explicitly if needed
         }
+        players += name
     }
 
-    fun playersList(): String {
-        return "players: ${players.joinToString(", ")}"
-    }
+    fun playersList(): String = "players: ${players.joinToString(", ")}"
 }

d9c35b104f6ea2d0dd1fcb353fbb53072abf9f19
Pietro Di Bello <pierodibello@gmail.com>
2024-05-03 23:40:32 +0200

    chore(gradle): setup junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,20 +2,17 @@ plugins {
     kotlin("jvm") version "1.9.23"
 }
 
-group = "com.xpepper"
-version = "1.0-SNAPSHOT"
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
 }

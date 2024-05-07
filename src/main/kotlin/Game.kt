ead2fa49e066d2f633d8781e17b9e2826b4dac83
Pietro Di Bello <pierodibello@gmail.com>
2024-05-07 23:03:58 +0200

    feat: add player movement and tracking in Game module
    - Implement `movePlayer` method to update and report player positions based on dice rolls.
    - Extend `GameTest` to include tests for the new `movePlayer` functionality, ensuring correct position updates and responses.

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,12 +1,24 @@
 class Game {
     private val players = mutableListOf<Player>()
+    private val positions = mutableMapOf<Player, Int>()
 
     fun addPlayer(player: Player): String {
         if (player.isAlreadyPresent()) {
             return "${player.name}: already existing player"
         }
         players.add(player)
+        positions[player] = 0
         return "players: ${playersInGame()}"
+    }
+
+    fun movePlayer(name: String, dice1: Int, dice2: Int): String {
+        val player = players.find { it.name == name } ?: return "Player not found"
+        val oldPosition = positions[player] ?: return "Player position not found"
+        val newPosition = oldPosition + dice1 + dice2
+        positions[player] = newPosition
+
+        val oldPositionName = if (oldPosition == 0) "Start" else oldPosition.toString()
+        return "$name rolls $dice1, $dice2. $name moves from $oldPositionName to $newPosition"
     }
 
     private fun Player.isAlreadyPresent() = players.any { it.name == name }
--- src/test/kotlin/GameTest.kt
+++ src/test/kotlin/GameTest.kt
@@ -16,4 +16,19 @@ class GameTest {
         val response = game.addPlayer(Player("Pippo"))
         assertEquals("Pippo: already existing player", response)
     }
+
+    @Test
+    fun `when a player moves, the system should update their position and respond with the appropriate message`() {
+        game.addPlayer(Player("Pippo"))
+        game.addPlayer(Player("Pluto"))
+
+        var response = game.movePlayer("Pippo", 4, 2)
+        assertEquals("Pippo rolls 4, 2. Pippo moves from Start to 6", response)
+
+        response = game.movePlayer("Pluto", 2, 2)
+        assertEquals("Pluto rolls 2, 2. Pluto moves from Start to 4", response)
+
+        response = game.movePlayer("Pippo", 2, 3)
+        assertEquals("Pippo rolls 2, 3. Pippo moves from 6 to 11", response)
+    }
 }

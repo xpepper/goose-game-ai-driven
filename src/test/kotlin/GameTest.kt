3e37a64b8013ba51fcdc0e77833d16c0aa84d906
Pietro Di Bello <pierodibello@gmail.com>
2024-05-07 22:51:15 +0200

    refactor(GameTest): move game instance to class level in tests

--- src/test/kotlin/GameTest.kt
+++ src/test/kotlin/GameTest.kt
@@ -2,17 +2,16 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class GameTest {
+    private val game = Game()
 
     @Test
     fun `when a player is added, the system should respond with the player's name`() {
-        val game = Game()
         val response = game.addPlayer(Player("Pippo"))
         assertEquals("players: Pippo", response)
     }
 
     @Test
     fun `when an existing player is added again, the system should respond with an appropriate message`() {
-        val game = Game()
         game.addPlayer(Player("Pippo"))
         val response = game.addPlayer(Player("Pippo"))
         assertEquals("Pippo: already existing player", response)

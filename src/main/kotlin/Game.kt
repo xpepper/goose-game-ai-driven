d48044cf970510e5b060948b40a538790ac4eeec
Pietro Di Bello <pierodibello@gmail.com>
2024-05-07 22:54:02 +0200

    refactor(Game.kt): extract player existence check into `isAlreadyPresent` method

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -2,12 +2,14 @@ class Game {
     private val players = mutableListOf<Player>()
 
     fun addPlayer(player: Player): String {
-        if (players.any { it.name == player.name }) {
+        if (player.isAlreadyPresent()) {
             return "${player.name}: already existing player"
         }
         players.add(player)
         return "players: ${playersInGame()}"
     }
+
+    private fun Player.isAlreadyPresent() = players.any { it.name == name }
 
     private fun playersInGame() = players.joinToString(", ") { it.name }
 }
